4461d596a52bdce160fc732f3a1b894d38c753e6
pierrot12 <pierrenoilhan58@hotmail.com>
2016-04-08 17:44:53 +0200

    New request

---
+++ SQL_Utils/ProcStock/SRC_ListerTablesColonnes.sql
@@ -0,0 +1,15 @@
+﻿DECLARE @tablename nvarchar(100)
+
+DECLARE curs CURSOR FOR SELECT name FROM sys.tables WHERE name like 'ACW%'
+OPEN curs
+FETCH NEXT FROM curs into @tablename
+WHILE @@FETCH_STATUS = 0
+BEGIN
+SELECT DISTINCT c.name as cname,
+    t.name  as tname
+        FROM sys.columns c 
+        LEFT OUTER JOIN sys.types as t ON c.user_type_id=t.user_type_id
+        where object_id=OBJECT_ID(@tablename)
+FETCH NEXT FROM curs into @tablename
+END
+CLOSE curs DEALLOCATE curs
